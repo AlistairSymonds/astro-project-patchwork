6d70ce36f7836d4c11f02bbe7853520acdfabccd
AlistairSymonds <alistair.symonds@gmail.com>
2022-09-14 23:33:02 +1000

    Update reproject benchmark for v0.10

--- reproject_benchmarking.py
+++ reproject_benchmarking.py
@@ -1,5 +1,4 @@
 from reproject import reproject_adaptive, reproject_interp, reproject_exact
-from reproject.utils import reproject_blocked
 from astropy.wcs import WCS
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,9 +7,13 @@ import multiprocessing
 import json
 
 def main():
-    processes = [False,1,2,4, True]
-    reproj_funcs = [reproject_interp, reproject_adaptive] #, reproject_exact]
-    block_sizes = [500, 1000, 2500]
+    # pick sizes here
+    rxsize=rysize=10000 #destination reporjection array size
+    xsize=ysize=1024
+
+    processes = [False, 1, 2, 4, True]
+    reproj_funcs = [reproject_interp, reproject_exact]
+    block_sizes = [None, [2500,2500],[rxsize, 100],[100, rxsize]]
     results = []
 
     mra=180.0
@@ -18,9 +21,7 @@ def main():
     cellsize=1.0/3600
     ctype=('RA---SIN','DEC--SIN')
 
-    # pick sizes here
-    rxsize=rysize=5000
-    xsize=ysize=1024
+
 
     rwcs=WCS(naxis=2)
     rwcs.wcs.ctype=ctype
@@ -45,16 +46,13 @@ def main():
         for num_p in processes:
             for bsize in block_sizes:
                 time_start = timer()
-                r, footprint = reproject_blocked(func,input_data=(data, header), output_projection=rheader,
-                                                 parallel=num_p, block_size=(bsize, bsize), return_footprint=True)
+                r, footprint = func(input_data=(data, header), output_projection=rheader,
+                                                 parallel=num_p, block_size=bsize, return_footprint=True)
                 time_taken = timer() - time_start
 
                 array_closenesss = np.isclose(reference_reprojection, r, equal_nan=True)
                 if np.any(array_closenesss == False):
                     print("arrays not close!")
-                    plt.figure()
-                    plt.imshow(array_closenesss)
-                    plt.show()
 
                 print("### Time taken was: " + str(time_taken) + " for func is:" +str(func) + ", Processes "+ str(num_p) + ", block size " + str(bsize))
                 results.append({'time': time_taken, 'func': str(func), 'processes': num_p, 'block_size':bsize})
